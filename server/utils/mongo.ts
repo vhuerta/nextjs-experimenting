0aa0952fe946856f334d1d62715f5a05de49c31a
Victor Huerta <vhuertahnz@gmail.com>
2021-04-21 20:05:22 -0500

    Mongo db url

--- server/utils/mongo.ts
+++ server/utils/mongo.ts
@@ -6,7 +6,7 @@ let conn: mongoose.Connection = null;
 
 export async function getConnection() {
   if (conn == null) {
-    conn = mongoose.createConnection("mongodb://localhost:27017/test", {
+    conn = mongoose.createConnection(process.env.NEXT_SERVER_MONGO_URI || "mongodb://localhost:27017/test", {
       bufferCommands: false,
       bufferMaxEntries: 0,
       useNewUrlParser: true,
